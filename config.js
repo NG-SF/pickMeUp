0e7a6e20b6050fe37737588489af3d6a49bf0bd1
Nataliya Gapochka <natasha.gapochka@gmail.com>
2018-01-13 20:26:47 -0800

    corrected models

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
-(function() {
-  'use strict';
+
   exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/jokes-app';
 
   exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/jokes-app';
 
   exports.PORT = process.env.PORT || 8080;
-})();
+
--- models.js
+++ models.js
@@ -1,24 +1,13 @@
-'use strict';
 const mongoose = require('mongoose');
 const moment = require('moment');
 
 mongoose.Promise = global.Promise;
 
-// represents schema for blog posts
 const jokeSchema = mongoose.Schema({
   title: { type: String, required: true },
   content: String,
   image: String,
-  author: {
-    firstName: String,
-    lastName: String
-  },
   created: { type: Date, default: Date.now() }
-});
-
-// virtual property to return full author name
-jokeSchema.virtual('authorName').get(function() {
-  return `${this.author.firstName} ${this.author.lastName}`.trim();
 });
 
 // virtual property to return date formated with moment.js
@@ -26,17 +15,7 @@ jokeSchema.virtual('publishDate').get(function() {
   return moment(this.created).format('MMMM Do YYYY');
 });
 
-// instance method that returns blog posts in format that we want
-jokeSchema.methods.serialize = function() {
-  return {
-    title: this.title,
-    content: this.content,
-    image: this.image,
-    author: this.authorName,
-    created: this.publishDate,
-    id: this._id
-  };
-};
-
 const Joke = mongoose.model('jokes', jokeSchema);
+
+
 module.exports = { Joke };
