f2c95aff64f3062b74b36e7aa23ca77e01e00adb
Nataliya Gapochka <natasha.gapochka@gmail.com>
2018-01-20 13:57:09 -0800

    linked mLab

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 
-  exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/jokes-app';
+  exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/jokes-app' || 'mongodb://blackSea:3blackSea509@ds261917.mlab.com:61917/jokes-app' ;
 
   exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/jokes-app-test';
 
