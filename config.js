11f51d67ec123cdd44108a8d6f2a6a60bff601a9
Nataliya Gapochka <natasha.gapochka@gmail.com>
2018-07-11 14:40:41 -0700

    update mongo connection

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
-  exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/jokes-app';
+  exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/jokes-app';
 
-  exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/jokes-app-test';
+  exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost:27017/jokes-app-test';
 
   exports.PORT = process.env.PORT || 8080;
 
--- server.js
+++ server.js
@@ -52,7 +52,7 @@ let server;
 // it is also used in integration tests.
 function runServer(databaseUrl = DATABASE_URL, port = PORT) {
   return new Promise((resolve, reject) => {
-    mongoose.connect(databaseUrl, err => {
+    mongoose.connect(databaseUrl, {useNewUrlParser: true }, err => {
       if (err) {
         return reject(err);
       }
